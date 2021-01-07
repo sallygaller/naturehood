91755f449e33101e50c2d26103b96f8a3cffc581
unknown <sallyannegaller@gmail.com>
2021-01-06 16:47:26 -0800

    Added script tags

--- src/AddObservationMap/AddObservationMap.js
+++ src/AddObservationMap/AddObservationMap.js
@@ -14,6 +14,19 @@ const center = {
   lng: -122.7606,
 };
 
+// Create the script tag, set the appropriate attributes
+var script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+script.defer = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function () {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+
 export default function AddObservationMap(props) {
   const getCoordinates = (e) => {
     const lat = e.latLng.lat().toFixed(3);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import API_KEY from "./config";
 
 import "./index.css";
 import App from "./App/App";
@@ -41,19 +40,6 @@ const OBSERVATIONS = [
     lng: -122.751,
   },
 ];
-
-// Create the script tag, set the appropriate attributes
-var script = document.createElement("script");
-script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
-script.defer = true;
-
-// Attach your callback function to the `window` object
-window.initMap = function () {
-  // JS API is loaded and available
-};
-
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
 
 ReactDOM.render(
   <BrowserRouter>
