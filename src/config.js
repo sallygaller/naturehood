284d72e105c73581bd63ae2ebaae5a3cf323a5b8
unknown <sallyannegaller@gmail.com>
2021-01-24 14:34:55 -0800

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const API_TOKEN = process.env.REACT_APP_API_KEY;
 const API_TOKEN_GOOGLE = process.env.REACT_APP_GOOGLE_TOKEN;
 const TOKEN_KEY = "test-auth-token";
-const API_ENDPOINT = `http://localhost:8000/api`;
+const API_ENDPOINT =
+  process.env.REACT_APP_BASE_URL || `http://localhost:8000/api`;
 
 export { API_TOKEN, API_TOKEN_GOOGLE, TOKEN_KEY, API_ENDPOINT };
