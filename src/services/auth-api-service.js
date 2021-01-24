f8e3fb929e74a66f2d30d6d3dd75091df835a046
unknown <sallyannegaller@gmail.com>
2021-01-24 15:50:09 -0800

    Changed auth-api-service

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -14,14 +14,13 @@ const AuthApiService = {
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
-  postLogin(email, password) {
-    console.log(email, password);
+  postLogin(credentials) {
     return fetch(`${API_ENDPOINT}/auth/login`, {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify(credentials),
     })
       .then((res) =>
         !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
